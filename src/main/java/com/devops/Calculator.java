ed4439a4abadbc62d7dfa24a69c27d3e3d8b42ca
DHYANESHWARANRx3468 <rxdheena143@gmail.com>
2026-03-10 20:28:59 +0000

    Add files via upload

---
+++ src/main/java/com/devops/Calculator.java
@@ -0,0 +1,18 @@
+package com.devops;
+
+public class Calculator {
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+    public double divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("--- DevOps Maven Calculator ---");
+        System.out.println("Addition (10+5): " + calc.add(10, 5));
+        System.out.println("Subtraction (10-5): " + calc.subtract(10, 5));
+    }
+}
---
+++ src/test/java/com/devops/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.devops;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(15, calc.add(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calc.subtract(10, 5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        calc.divide(10, 0);
+    }
+}
